69461086b754ed5b555e38fd096d489181949b67
michael-renw <ycjay19434@163.com>
2017-05-15 10:44:22 +0800

    5-15

--- michael_img/js/index.js
+++ michael_img/js/index.js
@@ -1,9 +1,18 @@
 var jQuery = jQuery.noConflict();
 
-jQuery('.user').mouseover(function(){
-	jQuery('.ui-login-status').css('display','block');
-});
 
-jQuery('.user').mouseout(function(){	
+
+jQuery(document).ready(function(){
+	jQuery('.user').mouseover(function(){
+	jQuery('.ui-login-status').css('display','block');
+	});
+
+	jQuery('.user').mouseout(function(){	
 		jQuery('.ui-login-status').css('display','none');
+	});
+	//折叠
+	jQuery('.btn-fold').on('click',function(e){
+		jQuery(e.target).parents('.foldableBox').toggleClass('fold');
+	});
+
 });
